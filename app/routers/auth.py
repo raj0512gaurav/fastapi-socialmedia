7682665ec0a0e5d36f3cbc0d46a3cf087da4d0bb
raj0512gaurav <raj99gaurav@gmail.com>
2023-12-14 12:02:27 +0530

    changes in auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models, utils, oauth2, schemas
 
-router = APIRouter(tags=['Authentication'])
+router = APIRouter(
+    prefix='/login'
+    tags=['Authentication'])
 
-@router.post("/login", response_model=schemas.Token)
+@router.post("/", response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
